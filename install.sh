f4fd3a3725632b96f6b71c3361ee37d5fba9f218
Theophane RUPIN <theophane.rupin@gmail.com>
2013-07-25 16:57:29 +0200

    install now permit a full installation

--- install.sh
+++ install.sh
@@ -1,12 +1,27 @@
 #! /bin/sh
 
+echo "cleaning ..."
+rm -rf .utils
 mkdir -p .utils
+rm -rf node_modules
+
+echo "downloading phantomjs ..."
 cd .utils
 
 rm -rf phantomjs
 wget -qO- https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 | tar -jxf -
 mv phantomjs-1.9.1-linux-x86_64 phantomjs
 
+echo "downloading casperjs ..."
 rm -rf casperjs
 wget -qO- https://github.com/n1k0/casperjs/archive/1.0.3.tar.gz | tar -zxf -
 mv casperjs-1.0.3 casperjs
+
+cd ..
+
+npm install
+
+cd node_modules
+git clone git@github.com:trupin/solidify.git
+cd solidify
+npm install
