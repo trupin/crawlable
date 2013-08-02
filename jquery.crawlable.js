cfe79da2e1ee13127eda56c626613dd76a0a0a31
Theophane RUPIN <theophane.rupin@gmail.com>
2013-08-02 14:46:44 +0200

    jquery.crawlable init improvements. crawlable now set up a new context to load the application.

--- jquery.crawlable.js
+++ jquery.crawlable.js
@@ -17,13 +17,14 @@
 
         var that = this;
 
-        that.options = options = options || {};
-
         options.wait = typeof options.wait == 'number' ? options.wait : 250;
         options.interval = typeof options.interval == 'number' ? options.interval : 20;
+        options.context = typeof options.context == 'string' ? '<div>' + options.context + '</div>' : '<div></div>';
 
-        that.$context = $(options.context || 'div');
-        that.$context.hide();
+        var $context = $(options.context);
+
+        if (!$context.length)
+            throw new Error('Couldn\'t fetch the context.');
 
         if ($.isCrawlable) {
             that.on('app:load', function () {
@@ -31,17 +32,24 @@
             });
         }
         else {
+            var jqInitFn = $.fn.init;
             that.on('app:load', function () {
-                that.remove();
-                that.$context.show();
+                that.html($context.children());
+                $context.remove();
+
+                $.fn.init = jqInitFn;
             });
+            // overload jquery in order to always use the app context and not window.
+            $.fn.init = function (selector, context) {
+                return new jqInitFn(selector, context, $context);
+            };
         }
 
         /**
          * Triggers the document:ajax:inactive event when there is no more pending ajax request at page load.
          * This event will be triggered only one time.
          */
-        var t, c = that.options.wait, called = false;
+        var t, c = options.wait, called = false;
 
         var onTimeout = function (i) {
             if (!called) {
@@ -54,11 +62,11 @@
         var i = setInterval(function () {
             if ($.active) {
                 if (t) clearTimeout(t);
-                t = setTimeout(_.bind(onTimeout, i), that.options.wait);
+                t = setTimeout(_.bind(onTimeout, i), options.wait);
             }
             else if (c <= 0) onTimeout(i);
-            c -= that.options.interval;
-        }, that.options.interval);
+            c -= options.interval;
+        }, options.interval);
 
         return that;
     };
