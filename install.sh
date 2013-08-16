d9a31523caaa6403dee9218497206216aaae264b
Theophane RUPIN <theophane.rupin@gmail.com>
2013-08-16 17:01:08 +0200

    remove casperjs from install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,6 @@ rm -rf phantomjs
 wget -qO- https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 | tar -jxf -
 mv phantomjs-1.9.1-linux-x86_64 phantomjs
 
-echo "downloading casperjs ..."
-rm -rf casperjs
-wget -qO- https://github.com/n1k0/casperjs/archive/1.0.3.tar.gz | tar -zxf -
-mv casperjs-1.0.3 casperjs
-
 cd ..
 
 npm install
