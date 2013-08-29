47e72622487450b58464ca56b85757d14ea2fff7
Theophane RUPIN <theophane.rupin@gmail.com>
2013-08-29 17:41:28 +0200

    fix solidify install

--- install.sh
+++ install.sh
@@ -13,4 +13,11 @@ rm -rf phantomjs
 wget -qO- https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-`uname -m`.tar.bz2 | tar -jxf -
 mv phantomjs-1.9.1-linux-`uname -m` phantomjs
 
+cd ..
+
+cd node_modules
+git clone git@github.com:trupin/solidify.git
+
+cd solidify
+npm install .
 
