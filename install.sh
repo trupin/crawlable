788b42143fd6c869979a19ffe8d45a9bf53799a7
Theophane RUPIN <theophane.rupin@gmail.com>
2013-08-29 18:18:32 +0200

    new version

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ echo "downloading phantomjs ..."
 cd .utils
 
 rm -rf phantomjs
-wget -qO- https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-`uname -m`.tar.bz2 | tar -jxf -
+
+curl -s https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-`uname -m`.tar.bz2 | tar -jxf -
+
 mv phantomjs-1.9.1-linux-`uname -m` phantomjs
 
 cd ..
